891d4aad4a6b8dfa057921d367f3f735249d1a36
Luís Ferreira <17674315+luismeruje@users.noreply.github.com>
2024-03-15 15:45:12 +0000

    Added parsing of results to test

--- disk-simulation/run-benchmarks.sh
+++ disk-simulation/run-benchmarks.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ITERATIONS=1
+ITERATIONS=5
 TEST_SIZE=1G
+SUM_THROUGHPUT=0
+SUM_IOPS=0
 
 read line #Skip header line
 while read line; do
@@ -12,11 +14,19 @@ while read line; do
     workload=$(echo $line | cut -d ' ' -f 5)
     for I in $(eval echo "{1..$ITERATIONS}")
     do
-        fio --name=write_throughput --directory=. --numjobs=1 --size=$TEST_SIZE --time_based --runtime=22s --ramp_time=2s  --direct=$direct --verify=0 --bs=$block --iodepth=$iodepth --rw=$workload --group_reporting=1
+        run=$(fio --minimal --name=test --directory=. --numjobs=1 --size=$TEST_SIZE --time_based --runtime=22s --ramp_time=2s  --direct=$direct --verify=0 --bs=$block --iodepth=$iodepth --rw=$workload --group_reporting=1)
+        run=$(echo "$run" | awk '$1 ~ /^[1-9]/')
+        #echo "$run"
+        throughput=$(echo "$run" | cut -d ';' -f 48)
+        iops=$(echo "$run" | cut -d ';' -f 49)
+        SUM_THROUGHPUT=$(echo "$SUM_THROUGHPUT + $throughput" | bc)
+        SUM_IOPS=$(echo "$SUM_IOPS + $iops" | bc)
     done
+    result_throughput=$(echo "scale=4; $SUM_THROUGHPUT / $ITERATIONS" | bc)
+    result_iops=$(echo "scale=4; $SUM_IOPS / $ITERATIONS" | bc)
+    echo "Test $name with $ITERATIONS iterations. Avg IOPS: $result_iops; Avg Throughput: $result_throughput"
+    SUM_THROUGHPUT=0
+    SUM_IOPS=0
 done
 
-
-
-
-
+rm test.0.0
