34066680307b3e4932fb7cbd8239d1ba69da3888
joaonovais181 <pg53907@alunos.uminho.pt>
2025-08-15 11:52:24 +0100

    script to save results updated

--- test-scripts/save-results.sh
+++ test-scripts/save-results.sh
@@ -1,7 +1,9 @@
 RESULTS_FOLDER=${1}
-TEST_PATH=${2}
-ORCH_FOLDER=${3:-/tmp/mulletbench-orch}
-PLOT_FOLDER=${4:-~/MulletBench/results-plot}
+IDENTIFIER=${2}
+TEST_PATH=${3}
+CONFIG_FILE=${4:-null}
+ORCH_FOLDER=${5:-/tmp/mulletbench-orch}
+PLOT_FOLDER=${6:-~/MulletBench/results-plot}
 
 if [ $# -eq 1 ]; then
     >&2 echo "Missing arguments"
@@ -11,9 +13,14 @@ fi
 echo "Clearing data folder" && rm -rf $PLOT_FOLDER/data && mkdir $PLOT_FOLDER/data
 echo "Clearing images folder" && rm -rf $PLOT_FOLDER/images && mkdir $PLOT_FOLDER/images
 echo "Copying test results" && cp $ORCH_FOLDER/results/$RESULTS_FOLDER/* $PLOT_FOLDER/data
-echo "Generating plots for test $RESULTS_FOLDER" && cd $PLOT_FOLDER && python3 plot.py
-mkdir $PLOT_FOLDER/$TEST_PATH
-echo "Moving test data to $TEST_PATH" && mv $PLOT_FOLDER/data $PLOT_FOLDER/$TEST_PATH
-echo "Moving test plots to $TEST_PATH" && mv $PLOT_FOLDER/images $PLOT_FOLDER/$TEST_PATH
-echo "Getting test results" && docker logs --tail 1500 mulletbench-orchestrator | awk 'BEGIN { found = 0 } /Test results:/ { output = ""; found = 1 } found { output = output $0 ORS } END { printf "%s", output }' > $PLOT_FOLDER/$TEST_PATH/results.txt
-echo "Removing original test results folder" && sudo rm -rf $ORCH_FOLDER/results/$RESULTS_FOLDER
+echo "Generating plots for test $RESULTS_FOLDER" && cd $PLOT_FOLDER && python3.9 plot.py
+mkdir -p $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH
+echo "Moving test data to $IDENTIFIER/$TEST_PATH" && mv $PLOT_FOLDER/data $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH
+echo "Moving test plots to $IDENTIFIER/$TEST_PATH" && mv $PLOT_FOLDER/images $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH
+echo "Getting test results" && docker logs --tail 1500 mulletbench-orchestrator | awk 'BEGIN { found = 0 } /Test results:/ { output = ""; found = 1 } found { output = output $0 ORS } END { printf "%s", output }' > $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH/results.txt
+echo "Removing original test results folder" | sudo -S rm -rf $ORCH_FOLDER/results/$RESULTS_FOLDER
+
+if [ "$CONFIG_FILE" != "null" ]; then
+    echo "Copying config file to $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH"
+    cp $CONFIG_FILE $PLOT_FOLDER/results/$IDENTIFIER/$TEST_PATH/../config.yaml
+fi
